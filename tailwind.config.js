711d0802806f8ac17319be6225c1efceb1b6963e
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-03 20:16:50 +0200

    Creation of the RecipeCard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,9 @@ module.exports = {
         secondary: "#FFE4E1",
         accent: "#F96140",
         neutral: "#F2F3F9",
-        neutral_50: "#ececec",
+        neutral50: "#ececec",
         dark: "#282e32",
-        dark_25: "#c9cbcc",
+        dark25: "#c9cbcc",
         darkmode: "#363F45",
         lightmode: "#FFFFFF",
       },
@@ -46,6 +46,11 @@ module.exports = {
           fontWeight: '500',
         }],
         'xs': ['0.75rem', {
+          lineHeight: '1.5',
+          fontWeight: '400',
+        }],
+        // test of the font for food categories
+        'xxs': ['0.5rem', {
           lineHeight: '1.5',
           fontWeight: '400',
         }],
