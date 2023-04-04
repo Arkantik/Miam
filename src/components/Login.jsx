6938189bc56a95f4c90ee03b54164923f6a26b69
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-04 14:49:10 +0200

    Added lines around 'Or continue with'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 const Login = () => (
 
-    <div className="text-dark bg-neutral50 blur-none rounded-3xl w-[300px] px-12 py-8">
+    <div className="text-dark bg-neutral50 blur-none rounded-3xl w-[300px] px-12 py-8">{/*Will need to blur or remove it*/}
         <div className="flex-col space-y-2">
             <h2 className="flex justify-center text-xl">Login</h2>
             <form className="space-y-2">
@@ -12,13 +12,17 @@ const Login = () => (
                     <label for="password" className="text-base text-dark">Password</label>
                     <div className="flex items-center">
                         <input type="password" className="w-full bg-white border rounded-md border-gray-300 focus:border-primary text-xs outline-none text-dark25 leading-5 py-1 px-3 transition-colors duration-200 ease-in-out"/>
-                        <img className="absolute right-16" src='/public/assets/icon/pass-show.svg'/>
+                        <img className="absolute right-16" src='/public/assets/icon/pass-show.svg'/> {/*Will need to fix the eye icon*/}
                     </div>
                     <label className="text-xs"><a href="#">Forgot password?</a></label>
                 </div>
                 <button className="text-white py-2 px-5 w-full bg-primary hover:bg-accent rounded">Sign in</button>
             </form>
-            <p className="flex justify-center text-xs">Or continue with</p>
+            <p className="relative flex justify-center text-xs">
+                <span class="line-login-before"></span>
+                Or continue with
+                <span class="line-login-after"></span>
+            </p>
             <div className="flex justify-center">
                 <button className="flex items-center justify-center rounded-3xl px-5 py-2.5 w-20 bg-white">
                     <img src="public/assets/icon/google.svg"/>
