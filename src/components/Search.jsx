8d50db390749f97dd82d3dd11ba4693e5985dbf5
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-09 18:39:44 +0200

    Search bar light/dark mode complete

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,23 @@
 export default function Search() {
     return (
 
-        <form>
+        <form className="relative">
             <div className="flex ">
-                <label htmlFor="search-dropdown" className="mb-2 text-sm sr-only ">Your Email</label>
-                <button id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm text-center bg-neutral50 border border-gray-300 rounded-l-lg hover:bg-dark25 focus:outline-none dark:bg-dark dark:hover:bg-gray-600 dark:text-neutral dark:border-dark25" type="button">All categories <svg aria-hidden="true" className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
-                    <div id="dropdown" className="hidden z-10 bg-neutral50 divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-dark">
-                        <ul className="py-2 text-sm " aria-labelledby="dropdown-button">
+                <label htmlFor="search-dropdown" className="mb-2 text-sm sr-only ">Search</label>
+                <button id="dropdown-button" data-dropdown-toggle="dropdown" className=" flex-shrink-0 inline-flex items-center text-center py-2.5 px-4 text-sm bg-neutral50 dark:bg-dark border border-dark25 dark:border-dark25 hover:bg-dark25 dark:hover:bg-gray-600 rounded-l-lg focus:outline-none " type="button">All categories <svg aria-hidden="true" className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
+                    <div id="dropdown" className="absolute top-full z-10 bg-neutral50 dark:bg-dark divide-y divide-gray-100 rounded-lg shadow w-40 ">
+                        <ul className="py-2 text-sm" aria-labelledby="dropdown-button">
                             <li>
-                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">Cuisine</button>
+                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-dark25 dark:hover:bg-gray-600 ">Cuisine</button>
                             </li>
                             <li>
-                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">Meal</button>
+                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-dark25 dark:hover:bg-gray-600 ">Meal</button>
                             </li>
                             <li>
-                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">Cocktail</button>
+                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-dark25 dark:hover:bg-gray-600 ">Cocktail</button>
                             </li>
                             <li>
-                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">Other</button>
+                                <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-dark25 dark:hover:bg-gray-600 ">Other</button>
                             </li>
                         </ul>
                     </div>
