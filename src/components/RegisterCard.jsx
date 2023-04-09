360ec3786cd0f169fd682c257e36a0e21de71661
Denis Dezest <denis.dezest@gmail.com>
2023-04-09 12:24:56 +0200

    Fixed console errors for the login and register pages, added blur to the login and register cards

--- src/components/RegisterCard.jsx
+++ src/components/RegisterCard.jsx
@@ -2,23 +2,23 @@ import { NavLink } from "react-router-dom";
 
 export default function RegisterCard() {
   return (
-    <div className="text-dark bg-neutral50 blur-none rounded-3xl w-[300px] px-12 py-8">
-      {/*Will need to blur or remove it*/}
+    <div className="text-dark bg-neutral50/30 backdrop-blur-sm rounded-3xl w-[300px] px-12 py-8">
       <div className="flex-col space-y-2">
         <h2 className="flex justify-center text-xl">Register</h2>
         <form className="space-y-2">
           <div>
-            <label for="email" className="text-base text-dark">
+            <label htmlFor="email" className="text-base text-dark">
               Email
             </label>
             <input
               type="email"
               placeholder="username@gmail.com"
               className="placeholder:text-dark25 placeholder:font-light w-full bg-white border rounded-md border-gray-300 focus:border-primary text-xs outline-none text-dark25 leading-5 py-1 px-3 transition-colors duration-300 ease-in-out"
+              id="email"
             />
           </div>
           <div>
-            <label for="password" className="text-base text-dark">
+            <label htmlFor="password" className="text-base text-dark">
               Password
             </label>
             <div className="space-y-2">
@@ -27,6 +27,7 @@ export default function RegisterCard() {
                   type="password"
                   placeholder="Password"
                   className="placeholder:text-dark25 placeholder:font-light w-full bg-white border rounded-md border-gray-300 focus:border-primary text-xs outline-none text-dark25 leading-5 py-1 pl-3 pr-9 duration-300 ease-in-out"
+                  id="password"
                 />
                 <img
                   className="absolute right-16"
@@ -55,9 +56,9 @@ export default function RegisterCard() {
           </button>
         </form>
         <p className="relative flex justify-center text-xs">
-          <span class="line-login-before"></span>
+          <span className="line-login-before"></span>
           Or continue with
-          <span class="line-login-after"></span>
+          <span className="line-login-after"></span>
         </p>
         <div className="flex justify-center">
           <button className="flex items-center justify-center rounded-3xl px-5 py-2.5 w-20 bg-white ring-1 ring-transparent hover:ring-accent duration-300 ease-in-out">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,6 @@ import LoginCard from "../components/LoginCard";
 export default function Login() {
   return (
     <div className="min-h-screen">
-      {/* Available height to be changed depending on the height of the navbar */}
       <div className="h-[calc(100vh-68px)] flex justify-center items-center">
         <LoginCard />
       </div>
