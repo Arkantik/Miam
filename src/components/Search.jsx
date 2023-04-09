2947a49f49ebfa89c22fd3de2f66f3b4449a099b
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-09 14:20:47 +0200

    Search bar change of the focus animation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -22,7 +22,7 @@ export default function Search() {
                         </ul>
                     </div>
                 <div className="relative w-full">
-                    <input type="search" id="search-dropdown" className="block p-2.5 w-full z-1 text-sm text-dark bg-dark25 rounded-r-lg border border-dark25 focus:outline-none focus:border-accent dark:bg-dark dark:border-l-dark25 dark:border-dark25 dark:placeholder-dark25 dark:text-white" placeholder="Search Mockups, Logos, Design Templates..." required/>
+                    <input type="search" id="search-dropdown" className="block p-2.5 w-full z-1 text-sm text-dark bg-dark25 rounded-r-lg border border-dark25 focus:outline-none focus:border-accent dark:bg-dark dark:border-l-dark25 dark:border-dark25 dark:placeholder-dark25 dark:text-white transition-colors duration-300 ease-in-out" placeholder="Search Mockups, Logos, Design Templates..." required/>
                     <button type="submit" className="absolute top-0 right-0 bottom-0 p-2.5 text-sm text-white bg-primary rounded-r-lg border border-accent hover:bg-accent focus:outline-none">
                         <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         <span className="sr-only">Search</span>
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -3,7 +3,7 @@ import RecipeCard from "/src/components/RecipeCard.jsx";
 
 export default function Recipes() {
   return (
-    <div className="recipes">
+    <div className="space-y-4">
       <h1 className="text-xl text-dark">Recipes</h1>
       <Search/>
       <RecipeCard />
