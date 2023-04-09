25ee5eac9965d0278b9d1469a478bbb3c13a09f7
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-09 13:19:05 +0200

    Tailwind flowbite added

---
+++ src/components/Search.jsx
@@ -0,0 +1,35 @@
+export default function Search() {
+    return (
+        
+        <form>
+            <div className="flex p-5">
+                <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+                <button id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-dark dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-dark25" type="button">All categories <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                        <li>
+                            <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
+                        </li>
+                        <li>
+                            <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
+                        </li>
+                        <li>
+                            <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
+                        </li>
+                        <li>
+                            <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
+                        </li>
+                    </ul>
+                </div>
+                <div className="relative w-full">
+                    <input type="search" id="search-dropdown" className="block p-2.5 w-full z-1 text-sm text-dark bg-gray-50 rounded-r-lg border border-gray-300 focus:ring-primary focus:border-accent dark:bg-dark dark:border-l-dark25  dark:border-dark25 dark:placeholder-dark25 dark:text-white dark:focus:border-accent" placeholder="Search Mockups, Logos, Design Templates..." required/>
+                    <button type="submit" className="absolute top-0 right-0 bottom-0 p-2.5 text-sm text-white bg-primary rounded-r-lg border border-accent hover:bg-accent focus:outline-none focus:ring-accent dark:bg-primary dark:hover:bg-accent dark:focus:ring-accent">
+                        <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        <span className="sr-only">Search</span>
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    )
+}
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,10 +1,13 @@
+import Search from "/src/components/Search.jsx";
 import RecipeCard from "/src/components/RecipeCard.jsx";
 
 export default function Recipes() {
   return (
     <div className="recipes">
-      <h1>Recipes</h1>
+      <h1 className="text-xl text-dark">Recipes</h1>
+      <Search/>
       <RecipeCard />
+      
     </div>
   );
 }
