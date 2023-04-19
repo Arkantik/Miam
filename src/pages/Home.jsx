345d4ede787824e10a43648313a7fd4f92a8a87b
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-04-19 20:05:59 +0200

    Creation of Carousel component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import Carousel from "../components/Carousel";
+
 export default function Home() {
   return (
     <section>
       <h2 className="text-xl">Home</h2>
+      <Carousel />
     </section>
   );
 }
