c4fa2b762c984b8dd801a7122b187be03e6571ae
Denis Dezest <denis.dezest@gmail.com>
2023-04-23 18:48:53 +0200

    Added feature Add to Favorite when clicking on the card 'like' button

--- src/components/RecipesLibrary.jsx
+++ src/components/RecipesLibrary.jsx
@@ -1,18 +1,15 @@
 import RecipeCard from "./RecipeCard.jsx";
 
-export default function RecipesLibrary({ recipes }) {
+export default function RecipesLibrary({ recipes, toggleFavoriteRecipe }) {
   return (
     <div className="gallery">
       {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} {...recipe} />
+        <RecipeCard
+          key={recipe.id}
+          {...recipe}
+          toggleFavoriteRecipe={toggleFavoriteRecipe}
+        />
       ))}
     </div>
   );
 }
-
-// <div className="grid grid-cols-1 gap-4 justify-items-center md:grid-cols-[_minmax(175px,_1fr)]">
-//
-
-// <div className="grid grid-cols-1 gap-4 justify-items-center md:grid-cols-2">
-//
-// <div className="flex flex-col items-center gap-4 md:flex-row md:flex-wrap">
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,14 +1,41 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 import Search from "../components/Search.jsx";
 import RecipesLibrary from "../components/RecipesLibrary";
 
-import recipes from "../data/data-recipes";
+import recipesFakeData from "../data/data-recipes";
 
 export default function Recipes() {
+  const [recipes, setRecipes] = useState(() => {
+    return [];
+  });
+
+  const toggleFavoriteRecipe = (id) => {
+    setRecipes((prevRecipes) =>
+      prevRecipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, favorite: !recipe.favorite } : recipe
+      )
+    );
+  };
+
+  // Create unique ID prior to using fake recipes datas
+  useEffect(() => {
+    const newRecipes = recipesFakeData.map((recipe) => ({
+      ...recipe,
+      id: uuidv4(),
+    }));
+    setRecipes(newRecipes);
+  }, []);
+
   return (
     <section className="space-y-4">
       <h2 className="text-xl">My Recipes Library</h2>
       <Search />
-      <RecipesLibrary recipes={recipes} />
+      <RecipesLibrary
+        recipes={recipes}
+        toggleFavoriteRecipe={toggleFavoriteRecipe}
+      />
     </section>
   );
 }
