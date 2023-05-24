b9456873e8eb7f6f63c3c9f936acce7c37db5139
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-24 19:58:27 +0200

    Removed h2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import RecipeDay from "../components/Home/RecipeDay";
 export default function Home() {
 	return (
 		<section className="space-y-8">
-			<h2 className="text-xl">Last Recipes</h2>
 			<Carousel />
 			<RecipeDay />
 		</section>
