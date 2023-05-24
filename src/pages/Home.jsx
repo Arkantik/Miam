20a9789b721af676c5d3aff2d9f3f98ebf6ac414
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-24 15:41:09 +0200

    Added title for recipe of the day

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import Carousel from "../components/Carousel";
+import Carousel from "../components/Home/Carousel";
 
 export default function Home() {
-  return (
-    <section className="space-y-4">
-      <h2 className="text-xl">Last Recipes</h2>
-      <Carousel />
-    </section>
-  );
+	return (
+		<section className="space-y-8">
+			<h2 className="text-xl">Last Recipes</h2>
+			<Carousel />
+			<h2 className="text-xl">Recipe of the day</h2>
+		</section>
+	);
 }
