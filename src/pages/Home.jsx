0623a2482ac4aed564ffcc78acb34c630c22f7d6
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-24 19:33:09 +0200

    Updated home page with RecipeDay component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Carousel from "../components/Home/Carousel";
+import RecipeDay from "../components/Home/RecipeDay";
 
 export default function Home() {
 	return (
 		<section className="space-y-8">
 			<h2 className="text-xl">Last Recipes</h2>
 			<Carousel />
-			<h2 className="text-xl">Recipe of the day</h2>
+			<RecipeDay />
 		</section>
 	);
 }
